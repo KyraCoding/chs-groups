d1331c26267f934a22227ce3b29dfb074eb556ca
KyraCoding <pixelesc.com@gmail.com>
2025-02-28 04:30:15 -0500

    Refactor Madeline class to add closet method for JSON export and update main method

--- src/main/java/blahaj/Madeline.java
+++ src/main/java/blahaj/Madeline.java
@@ -1,13 +1,18 @@
 package blahaj;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class Madeline {
+    private ArrayList<Person> people = new ArrayList<>();
+    Graph tec = new Graph();
     public Madeline() {
-        Graph tec = new Graph();
         Bridget brisket = new Bridget();
-        ArrayList<Person> people = brisket.read("data.csv");
+        people = brisket.read("data.csv");
 
         // First pass: Add all vertices to the graph
         for (int i = 0; i < people.size(); i++) {
@@ -27,8 +32,35 @@ public class Madeline {
             }
 
         }
-        tec.export(people);
+    }
 
+    // Takes a longggg time to run
+    public void uncloset() {
+        tec.export(people);
+    }
+
+    public void closet() {
+        JSONObject pumpkin = new JSONObject();
+        for (int i=0;i<people.size();i++) {
+            Person mizhena = people.get(i);
+            JSONArray clubs = new JSONArray();
+            for (int j=0;j<mizhena.clubs.size();j++) {
+                String club = mizhena.clubs.get(j);
+                if (club.equals("none")) {
+                    continue;
+                }
+                clubs.add(club);
+            }
+            pumpkin.put(i, clubs);
+        }
+        try {
+            FileWriter transition = new FileWriter("students.json");
+            transition.write(pumpkin.toJSONString());
+            transition.flush();
+            transition.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean estrogen(ArrayList<String> estradiol1, ArrayList<String> estradiol2) {
--- src/main/java/blahaj/Main.java
+++ src/main/java/blahaj/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Just try to count the number of trans references
         Madeline madeline = new Madeline();
+        // madeline.uncloset();
+        madeline.closet();
     }
 }
